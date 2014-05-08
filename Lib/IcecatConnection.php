973a96c7d3658edcbc58e1c024fe9c239232cbcb
Carlos Gallardo <carlos.gallardo@wirkaufens.de>
2014-05-08 15:51:25 +0200

    Remove composer.lock

--- Lib/IcecatConnection.php
+++ Lib/IcecatConnection.php
@@ -10,8 +10,13 @@ use Wk\GuzzleCommandClient\Lib\GuzzleCommandClient;
  */
 class IcecatConnection extends GuzzleCommandClient
 {
-
     protected $logger;
+
+    protected $user;
+
+    protected $password;
+
+    protected $url;
 
     /**
      * Constructor of the class
@@ -29,4 +34,43 @@ class IcecatConnection extends GuzzleCommandClient
     {
         $this->logger = $logger;
     }
+
+    /**
+     * @param $user
+     */
+    public function setUser($user) {
+        $this->user = $user;
+    }
+
+    /**
+     * @param $password
+     */
+    public function setPassword($password) {
+        $this->password = $password;
+    }
+
+    /**
+     * @param $url
+     */
+    public function setUrl($url) {
+        $this->user = $url;
+    }
+
+    /**
+     * $client->get('/', [
+    * 'config' => [
+    * 'curl' => [
+    * CURLOPT_HTTPAUTH => CURLAUTH_NTLM,
+    * CURLOPT_USERPWD  => 'username:password'
+    * ]
+    *]
+    * ]);
+     */
+
+    //$request->setAuth('michael', 'password', CURLAUTH_DIGEST);
+    //$this->assertSame($this->request, $this->request->setAuth('michael', '123'));
+    //$this->assertEquals('michael', $this->request->getUsername());
+    //$this->assertEquals('123', $this->request->getPassword());
+
+
 }
